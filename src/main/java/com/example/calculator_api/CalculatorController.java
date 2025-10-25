156f872378c8fe3a3f52d2d033396e142e17c0dd
Meerthika <meerthikasrrajendrans@gmail.com>
2025-10-25 15:01:56 +0530

    Add test cases

--- src/main/java/com/example/calculator_api/CalculatorController.java
+++ src/main/java/com/example/calculator_api/CalculatorController.java
@@ -1,5 +1,31 @@
 package com.example.calculator_api;
 
-public class CalculatorController {
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api")
+public class CalculatorController {
+	
+	@GetMapping("/add")
+	public int add(@RequestParam int a, @RequestParam int b) {
+		return a+b;
+	}
+	
+	
+	@GetMapping("/sub")
+	public int sub(@RequestParam int a, @RequestParam int b) {
+		return a-b;
+	}
+	
+	
+	@GetMapping("/multi")
+	public int multi(@RequestParam int a, @RequestParam int b) {
+		return a+b;
+	}
+	
+	@GetMapping("/div")
+	public int divide(@RequestParam int a, @RequestParam int b) {
+		return a+b;
+	}
+	
 }
--- src/test/java/com/example/calculator_api/CalculatorApiApplicationTests.java
+++ src/test/java/com/example/calculator_api/CalculatorApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.calculator_api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalculatorApiApplicationTests {
-
+	
+	CalculatorController controller = new CalculatorController();
+	
 	@Test
-	void contextLoads() {
-	}
+    void testAdd() {
+        assertEquals(8, controller.add(5, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(2, controller.sub(5, 3));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(15, controller.multi(5, 3));
+    }
 
 }
